d938e6fbaac75faddc8db48fac92a7253fcefe4c
Razib-Debnath16 <razib.debnath016@gmail.com>
2022-05-03 22:44:21 +0600

    adding items

--- src/Pages/Product/Product.js
+++ src/Pages/Product/Product.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Button, Card } from 'react-bootstrap';
 
-const Product = () => {
+const Product = (params) => {
+
+    const { name, img, description, price } = params.product;
     return (
         <div>
-
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={img} />
+                <Card.Body>
+                    <Card.Title>{name}</Card.Title>
+                    <Card.Text>
+                        {description}
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
         </div>
     );
 };
--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
+import './Products.css';
 
 const Products = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/products')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
     return (
-        <div>
-
+        <div className='product-container'>
+            {
+                products.map(product => <Product key={product._id} product={product}></Product>)
+            }
         </div>
     );
 };
