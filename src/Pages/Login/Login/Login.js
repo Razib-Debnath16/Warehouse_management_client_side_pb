0fed3227f7361a9ae2112f7dee8179d9521006d0
Razib-Debnath16 <razib.debnath016@gmail.com>
2022-05-03 13:40:58 +0600

    Adding login page with password verification

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,16 +1,18 @@
 import React, { useRef, useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
 import { async } from '@firebase/util';
 import { toast } from 'react-toastify';
+import SocialLogin from '../SocialLogin/SocialLogin';
 
 const Login = () => {
     const [agree, setAgree] = useState(false);
     const emailRef = useRef('');
     const passwordRef = useRef('');
     const navigate = useNavigate();
+    const location = useLocation();
     const [
         signInWithEmailAndPassword,
         user,
@@ -39,33 +41,53 @@ const Login = () => {
             toast('Please enter your email');
         }
     }
+
+    const from = location.state?.from?.pathname || "/";
+    if (user) {
+        navigate(from, { replace: true });
+    }
+    if (loading) {
+
+    }
+    let errorMessage;
+    if (error) {
+        errorMessage = <div>
+            <p>Error: {error.message}</p>
+        </div>
+    }
+
     const navigateRegister = () => {
         navigate('/login');
     }
     return (
         <div>
-            <h2 className='text-primary text-center'>Please Login</h2>
-            <Form onSubmit={handleLoginSubmit} className='container w-50 mx-auto'>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
-                </Form.Group>
+            <div>
+                <h2 className='text-primary text-center'>Please Login</h2>
+                <Form onSubmit={handleLoginSubmit} className='container w-50 mx-auto'>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
+                    </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check className={agree ? 'text-primary' : 'text-danger'} onClick={() => setAgree(!agree)} type="checkbox" label="Accept terms and conditions?" />
-                </Form.Group>
-                <Button disabled={!agree} variant="primary" type="submit">
-                    Login
-                </Button>
-                <p className='my-2'>Don't have an Account? <Link onClick={navigateRegister} className='text-primary text-decoration-none' to="/register">Please Register...</Link></p>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                        <Form.Check className={agree ? 'text-primary' : 'text-danger'} onClick={() => setAgree(!agree)} type="checkbox" label="Accept terms and conditions?" />
+                    </Form.Group>
+                    <Button disabled={!agree} variant="primary" type="submit">
+                        Login
+                    </Button>
+                    <p className='my-2'>Don't have an Account? <Link onClick={navigateRegister} className='text-primary text-decoration-none' to="/register">Please Register...</Link></p>
 
-                <p className='my-2'>Forget your Password? <Button onClick={ResetPassword} variant="link" className='text-primary text-decoration-none' >Reset Password... </Button></p>
-                <p className='text-danger'></p>
-            </Form>
+                    <p className='my-2'>Forget your Password? <Button onClick={ResetPassword} variant="link" className='text-primary text-decoration-none' >Reset Password... </Button></p>
+                    <p className='text-danger'>{errorMessage}</p>
+                </Form>
+            </div>
+            <div>
+                <SocialLogin></SocialLogin>
+            </div>
         </div>
     );
 };
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -3,17 +3,17 @@ import { Button, Form } from 'react-bootstrap';
 import { useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth';
 import { Link, useNavigate } from 'react-router-dom';
 import auth from '../../../firebase.init';
+import SocialLogin from '../SocialLogin/SocialLogin';
 
 const Register = () => {
     const navigate = useNavigate();
     const [agree, setAgree] = useState(false);
-    const [updateProfile, updating] = useUpdateProfile(auth);
+    const [updateProfile] = useUpdateProfile(auth);
     const [
         createUserWithEmailAndPassword,
-        user,
         loading,
         error,
-    ] = useCreateUserWithEmailAndPassword(auth);
+    ] = useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
 
     const handleRegisterSubmit = async (event) => {
         event.preventDefault();
@@ -26,34 +26,49 @@ const Register = () => {
         console.log('Updated profile');
         navigate('/home');
     }
+    if (loading) {
+
+    }
+    let errorMessage;
+    if (error) {
+        errorMessage = <div>
+            <p>Error: {error.message}</p>
+        </div>
+    }
     const navigateLogin = () => {
         navigate('/login');
     }
     return (
         <div>
-            <h2 className='text-primary text-center'>Please Register</h2>
-            <Form onSubmit={handleRegisterSubmit} className='container w-50 mx-auto'>
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Name :</Form.Label>
-                    <Form.Control type="name" name='name' placeholder="Name" required />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" name='email' placeholder="Enter email" required />
-                </Form.Group>
+            <div>
+                <h2 className='text-primary text-center'>Please Register</h2>
+                <Form onSubmit={handleRegisterSubmit} className='container w-50 mx-auto'>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Name :</Form.Label>
+                        <Form.Control type="name" name='name' placeholder="Name" required />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" name='email' placeholder="Enter email" required />
+                    </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" name='password' placeholder="Password" required />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check className={agree ? 'text-primary' : 'text-danger'} onClick={() => setAgree(!agree)} type="checkbox" label="Wanna Register ?" />
-                </Form.Group>
-                <Button disabled={!agree} variant="primary" type="submit">
-                    Register
-                </Button>
-                <p className='my-2'>Already have an account? <Link onClick={navigateLogin} className='text-primary text-decoration-none' to="/login" >Please Login </Link></p>
-            </Form>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" name='password' placeholder="Password" required />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                        <Form.Check className={agree ? 'text-primary' : 'text-danger'} onClick={() => setAgree(!agree)} type="checkbox" label="Wanna Register ?" />
+                    </Form.Group>
+                    <Button disabled={!agree} variant="primary" type="submit">
+                        Register
+                    </Button>
+                    <p className='my-2'>Already have an account? <Link onClick={navigateLogin} className='text-primary text-decoration-none' to="/login" >Please Login </Link></p>
+                    <p className='text-danger'>{errorMessage}</p>
+                </Form>
+            </div>
+            <div>
+                <SocialLogin></SocialLogin>
+            </div>
         </div>
     );
 };
